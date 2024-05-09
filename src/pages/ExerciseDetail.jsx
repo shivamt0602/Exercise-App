b31c55e387dd31519d97d50ef4574d33a9670e90
Pradip <pradip@leo.resnet.iitb.ac.in>
2024-05-10 00:35:52 +0530

    positioned the cards to look good

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,7 +1,12 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Box from '@mui/material/Box';
-import { Grid, Typography } from "@mui/material";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 
 const ExerciseDetail = () => {
@@ -28,40 +33,206 @@ const ExerciseDetail = () => {
 
     return (
         <>
-            <Box sx={{backgroundColor:"yellow"}}>
-                <Carousel responsive={responsive} sx={{display : "flex",justifyContent : "space-between"}}>
-                    <Grid container>
-                        <Grid item md={2}>
-                            <Typography>
-                                Item 1
-                            </Typography>
-                        </Grid>
-                        <Grid item md={2}>
-                            <Typography>
-                                Item 2
-                            </Typography>
-                        </Grid>
-                        <Grid item md={2}>
-                            <Typography>
-                                Item 3
-                            </Typography>
-                        </Grid>
-                        <Grid item md={2}>
-                            <Typography>
-                                Item 4
-                            </Typography>
-                        </Grid>
-                        <Grid item md={2}>
-                            <Typography>
-                                Item 5
-                            </Typography>
-                        </Grid>
-                        <Grid item md={2}>
-                            <Typography>
-                                Item 6
-                            </Typography>
-                        </Grid>
-                    </Grid>
+            <Box>
+                <Carousel responsive={responsive}>
+                    <div>
+                        <Card sx={{ minWidth: 275,backgroundColor:"#378CE7"}}>
+                            <CardContent sx={{textAlign:"center"}}>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    benevolent
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+
+                    <div>
+                        <Card sx={{ minWidth: 275,backgroundColor:"#378CE7"}}>
+                            <CardContent sx={{textAlign:"center"}}>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    benevolent
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+
+
+                    <div>
+                        <Card sx={{ minWidth: 275,backgroundColor:"#378CE7"}}>
+                            <CardContent sx={{textAlign:"center"}}>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    benevolent
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+
+
+                    <div>
+                        <Card sx={{ minWidth: 275,backgroundColor:"#378CE7"}}>
+                            <CardContent sx={{textAlign:"center"}}>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    benevolent
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+
+
+                    <div>
+                        <Card sx={{ minWidth: 275,backgroundColor:"#378CE7"}}>
+                            <CardContent sx={{textAlign:"center"}}>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    benevolent
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+
+
+                    <div>
+                        <Card sx={{ minWidth: 275,backgroundColor:"#378CE7"}}>
+                            <CardContent sx={{textAlign:"center"}}>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    benevolent
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+
+
+                    <div>
+                        <Card sx={{ minWidth: 275,backgroundColor:"#378CE7"}}>
+                            <CardContent sx={{textAlign:"center"}}>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    benevolent
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+
+
+                    <div>
+                        <Card sx={{ minWidth: 275,backgroundColor:"#378CE7"}}>
+                            <CardContent sx={{textAlign:"center"}}>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    benevolent
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+
                 </Carousel>
             </Box>
         </>
