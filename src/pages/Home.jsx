a8154df0d2678ad8b08397e6cc40fec4348f7807
Pradip <pradip@leo.resnet.iitb.ac.in>
2024-05-12 16:11:51 +0530

    used other api url to get list of exercises

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,14 +12,16 @@ const Home = () => {
 
     const [items, Setitems] = useState([])
     const [search, Setsearch] = useState("")
-    const [identifier, setIdentifier] = useState("")
-    const [exercisename, setExerciseName] = useState("")
-    
+
+
 
 
     const LoadExercises = async () => {
 
-        const url = `https://exercisedb.p.rapidapi.com/exercises/${identifier}/${exercisename}?limit=100`;
+        console.log("inside the loadexercises function")
+
+
+        const url = `https://exercisedb.p.rapidapi.com/exercises/name/${search}?limit=100`;
         console.log(url)
         const options = {
             method: 'GET',
@@ -33,6 +35,7 @@ const Home = () => {
             const response = await fetch(url, options);
             const result = await response.json();
             console.log(result)
+            Setsearch("")
 
         } catch (error) {
             console.error(error);
@@ -42,65 +45,19 @@ const Home = () => {
 
 
     const handleChange = (event) => {
-        const new_val = event.target.value;
+        const new_val = event.target.value.toLowerCase();
+        // new_val = new_val.toLowerCase();
         Setsearch(new_val)
     }
 
 
-
-    const fetchVariousExercises = async () => {
-
-
-        const url = 'https://exercisedb.p.rapidapi.com/exercises?limit=100';
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-                'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-            }
-        };
-
-        try {
-            const response = await fetch(url, options);
-            const result = await response.json();
-
-            const reqList = result.filter((data) => {
-                return (data.name.includes(search) || data.bodyPart.includes(search) || data.equipment.includes(search) || data.target.includes(search))
-            })
-
-
-            const reqObj = reqList[0];
-            console.log(reqObj);
-            
-            for (let key in reqObj) {
-                const value = reqObj[key]
-                if(value === search){
-                    console.log(key)
-                    console.log(value)
-                    setIdentifier(key)
-                    setExerciseName(value)
-                }
-            }
-        
-
-
-        } catch (error) {
-            console.error(error);
-        }
-
-    }
-
-
-
-    const handleClick = async() => {
-
-        await fetchVariousExercises()
+    const handleClick = async () => {
 
         await LoadExercises()
 
     }
 
-   
+
 
     const getExercices = async () => {
 
