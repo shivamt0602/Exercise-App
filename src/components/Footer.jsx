febfba8ab3c6012320eff405c6cb32e7167e4bd4
Pradip <pradip@leo.resnet.iitb.ac.in>
2024-05-18 18:03:58 +0530

    added footer so finally done until more queries

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,20 @@
+import React from 'react';
+import { Typography, Container, Grid, Link } from '@mui/material';
 
-const Footer = ()=>{
+const Footer = () => {
     return (
-        <>
-        Footer
-        </>
-    )
-}
+        <footer style={{ marginTop: '50px', backgroundColor: '#f0f0f0', padding: '20px 0' }}>
+            <Container>
+                <Grid container justifyContent="center" alignItems="center">
+                    <Grid item>
+                        <Typography variant="body1" color="textSecondary" align="center">
+                            © 2024 Gym App. All rights reserved.
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+        </footer>
+    );
+};
 
 export default Footer;
